013535e2d6a63d6b8ac1a0005d6594afcf323bf6
wintongee <wgee01@calpoly.edu>
2022-11-28 13:16:07 -0800

    Init

---
+++ Lab7/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    String jdbcUrl = "jdbc:mysql://db.labthreesixfive.com/wgee01?autoReconnect=true&useSSL=false";
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
