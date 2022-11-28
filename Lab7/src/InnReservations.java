46304f501331fc0bbf152d3fd3a17283b2d62b3a
wintongee <wgee01@calpoly.edu>
2022-11-28 13:40:40 -0800

    Update

--- Lab7/src/InnReservations.java
+++ Lab7/src/InnReservations.java
@@ -1,9 +1,12 @@
-public class Main {
+public class InnReservations {
 
     String jdbcUrl = "jdbc:mysql://db.labthreesixfive.com/wgee01?autoReconnect=true&useSSL=false";
+    String dbUsername = System.getenv("wgee01");
+    String dbPassword = System.getenv("APP_JDBC_PW");
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
 
 
     }
