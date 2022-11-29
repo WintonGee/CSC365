6b8317f97315bdca3c798ce1fd39d424760d8a4e
wintongee <wgee01@calpoly.edu>
2022-11-29 12:28:08 -0800

    Get connections to work

--- Lab7/src/InnReservations.java
+++ Lab7/src/InnReservations.java
@@ -1,14 +1,44 @@
-public class InnReservations {
+import java.sql.*;
 
-    String jdbcUrl = "jdbc:mysql://db.labthreesixfive.com/wgee01?autoReconnect=true&useSSL=false";
-    String dbUsername = System.getenv("wgee01");
-    String dbPassword = System.getenv("APP_JDBC_PW");
+public class InnReservations {
 
     public static void main(String[] args) {
 
+        try {
+            InnReservations hp = new InnReservations();
+//            int demoNum = Integer.parseInt(args[0]);
+            int demoNum = 1;
+            switch (demoNum) {
+                case 1 -> hp.demo1();
+//                case 2 -> hp.demo2();
+//                case 3 -> hp.demo3();
+//                case 4 -> hp.demo4();
+//                case 5 -> hp.demo5();
+            }
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+        } catch (Exception e2) {
+            System.err.println("Exception: " + e2.getMessage());
+        }
+    }
 
-
-
+    private void demo1() throws SQLException {
+        // Step 1: Establish connection to RDBMS
+        try (Connection conn = DriverManager.getConnection(
+                ConnectionData.JDBC_URL.s,
+                ConnectionData.DB_USER.s,
+                ConnectionData.DB_PASSWORD.s)) {
+            Statement statement = conn.createStatement();
+            statement.execute("show tables");
+            ResultSet rs = statement.executeQuery("SELECT * FROM airlines");
+            while (rs.next()) {
+                String id = rs.getString("Id");
+                String airline = rs.getString("Airline");
+                String abbreviation = rs.getString("Abbreviation");
+                String country = rs.getString("Country");
+                System.out.println(id + ", " + airline + ", " + abbreviation + ", " + country);
+            }
+        }
     }
 
 }
